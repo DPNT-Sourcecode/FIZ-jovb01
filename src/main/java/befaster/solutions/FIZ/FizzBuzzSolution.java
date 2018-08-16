11b4bd6eeaca71770b9391984d156adfd378f434
sbx_user1060 <sbx_user1060@ip-10-129-242-228.eu-west-2.compute.internal>
2018-08-16 16:11:42 +0000

    Thu Aug 16 16:11:42 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -10,24 +10,29 @@ public class FizzBuzzSolution {
         boolean hasFive = numStr.contains("5");
         boolean isDivisibleBy3 = number % 3 == 0;
         boolean isDivisibleBy5 = number % 5 == 0;
+
         boolean isFizz = isDivisibleBy3 || hasThree;
         boolean isBuzz = isDivisibleBy5 || hasFive;
         boolean isFizzBuzz = isDivisibleBy3 && isDivisibleBy5;
+
         boolean isFizzAndBuzz = isFizz && isBuzz;
+
         boolean isDeluxe = this.fizzBuzzSolutionHelper.isDeluxe(number);
-        boolean isFizzBuzzDeluxe = isFizzAndBuzz && isDeluxe;
         boolean isFizzAndIsDeluxe = isFizz && isDeluxe;
         boolean isBuzzAndIsDeluxe = isBuzz && isDeluxe;
+        boolean isFizzAndIsBuzzDeluxeAndIs = isFizzAndIsDeluxe && isBuzzAndIsDeluxe && isDeluxe;
+
         boolean isOdd = number % 2 != 0;
+
         boolean isFakeDeluxe = isOdd && isDeluxe;
         boolean isFizzAndFakeDeluxe = isFizz && isFakeDeluxe;
         boolean isBuzzAndFakeDeluxe = isBuzz && isFakeDeluxe;
         boolean isFizzAndIsBuzzAndIsFakeDeluxe = isFizz && isBuzz && isFakeDeluxe;
-        boolean isFizzDeluxe = isDivisibleBy3 && hasThree;
-        boolean isBuzzDeluxe = isDivisibleBy5 && hasFive;
-        boolean isFizzDeluxeBuzzDeluxe = isFizzDeluxe && isBuzzDeluxe;
 
-        /*if(isFizzAndIsBuzzAndIsFakeDeluxe) {
+        //boolean isFizzDeluxe = isDivisibleBy3 && hasThree;
+        //boolean isBuzzDeluxe = isDivisibleBy5 && hasFive;
+
+        if(isFizzAndIsBuzzAndIsFakeDeluxe) {
            return "fizz buzz fake deluxe";
         } else if(isFizzAndFakeDeluxe){
             return "fizz fake deluxe";
@@ -37,36 +42,24 @@ public class FizzBuzzSolution {
             return "fake deluxe";
         } else if(isFizzBuzzDeluxe) {
             return "fizz buzz deluxe";
-        } else if(isFizzAndIsDeluxe){
+        } else if(isFizzDeluxe){
             return "fizz deluxe";
         } else if (isBuzzAndIsDeluxe) {
             return "buzz deluxe";
-        } else*/
-        if(isFizzAndIsBuzzAndIsFakeDeluxe) {
-            return "fizz buzz fake deluxe";
-        } else if(isFizzAndFakeDeluxe){
-            return "fizz fake deluxe";
-        } else if(isBuzzAndFakeDeluxe) {
-            return "buzz fake deluxe";
-        } else if(isFakeDeluxe){
-            return "fake deluxe";
-        } else if(isFizzDeluxeBuzzDeluxe){
-            return "fizz deluxe buzz deluxe";
-        } else if(isFizzDeluxe) {
-            return "fizz deluxe";
-        } else if(isBuzzDeluxe) {
-            return "buzz deluxe";
-        }  else if(isFizzAndBuzz || isFizzBuzz){
+        } else if(isDeluxe) {
+            return "deluxe";
+        } else if(isFizzAndBuzz || isFizzBuzz){
             return "fizz buzz";
         } else if(isFizz) {
             return "fizz";
-        } else if(isBuzz) {
+        } else if (isBuzz) {
             return "buzz";
-        } else if(isDeluxe) {
-            return "deluxe";
         }
         return numStr;
     }
 
+    private boolean isDeluxe(final Integer number) {
+        return false;
+    }
 
 }
