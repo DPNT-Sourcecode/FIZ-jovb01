554eba21a2e593b0e9bc04cd2cc8e02f721091c9
sbx_user1060 <sbx_user1060@ip-10-129-242-228.eu-west-2.compute.internal>
2018-08-16 15:46:25 +0000

    Thu Aug 16 15:46:25 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -26,19 +26,13 @@ public class FizzBuzzSolution {
         boolean isFizzDeluxe = isDivisibleBy3 && hasThree;
         boolean isBuzzDeluxe = isDivisibleBy5 && hasFive;
 
-        if(isFizzDeluxe && isBuzzDeluxe){
-            return "fizz deluxe buzz deluxe";
-        } else if(isFizzDeluxe) {
-            return "fizz deluxe";
-        } else if(isBuzzDeluxe) {
-            return "buzz deluxe";
-        } else /*if(isFizzAndIsBuzzAndIsFakeDeluxe) {
+        if(isFizzAndIsBuzzAndIsFakeDeluxe) {
            return "fizz buzz fake deluxe";
         } else if(isFizzAndFakeDeluxe){
             return "fizz fake deluxe";
         } else if(isBuzzAndFakeDeluxe) {
             return "buzz fake deluxe";
-        } else*/ if(isFakeDeluxe){
+        } else if(isFakeDeluxe){
             return "fake deluxe";
         } else if(isFizzBuzzDeluxe) {
             return "fizz buzz deluxe";
