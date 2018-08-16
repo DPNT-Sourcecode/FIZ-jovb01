1f41c917a37885aae1ce48d863222af2e3d30f0d
sbx_user1061 <sbx_user1061@ip-10-128-223-115.eu-west-2.compute.internal>
2018-08-16 12:51:57 +0000

    Thu Aug 16 12:51:57 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -41,6 +41,27 @@ public class FizzBuzzSolutionTest {
     }
 
     @Test
+    public void shouldExpectFizzWhenNumberContains3() {
+        Assertions.assertThat(this.sut.fizzBuzz(3)).isEqualTo("fizz");
+        Assertions.assertThat(this.sut.fizzBuzz(13)).isEqualTo("fizz");
+        Assertions.assertThat(this.sut.fizzBuzz(23)).isEqualTo("fizz");
+        Assertions.assertThat(this.sut.fizzBuzz(32)).isEqualTo("fizz");
+        Assertions.assertThat(this.sut.fizzBuzz(33)).isEqualTo("fizz");
+        Assertions.assertThat(this.sut.fizzBuzz(43)).isEqualTo("fizz");
+        Assertions.assertThat(this.sut.fizzBuzz(343)).isEqualTo("fizz");
+    }
+
+    @Test
+    public void shouldExpectFizzWhenNumberContains5() {
+        Assertions.assertThat(this.sut.fizzBuzz(5)).isEqualTo("buzz");
+        Assertions.assertThat(this.sut.fizzBuzz(25)).isEqualTo("buzz");
+        Assertions.assertThat(this.sut.fizzBuzz(50)).isEqualTo("buzz");
+        Assertions.assertThat(this.sut.fizzBuzz(55)).isEqualTo("buzz");
+        Assertions.assertThat(this.sut.fizzBuzz(752)).isEqualTo("buzz");
+        Assertions.assertThat(this.sut.fizzBuzz(959)).isEqualTo("buzz");
+    }
+
+    @Test
     public void shouldExpectFizzBuzzWhenIsFizzAndIsBuzz() {
         Assertions.assertThat(this.sut.fizzBuzz(456)).isEqualTo("fizz buzz");
         Assertions.assertThat(this.sut.fizzBuzz(546)).isEqualTo("fizz buzz");
