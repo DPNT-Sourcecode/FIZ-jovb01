8eb08be7083540cdb187119a70f54c0247187600
sbx_user1061 <sbx_user1061@ip-10-129-35-100.eu-west-2.compute.internal>
2018-08-16 12:24:55 +0000

    Thu Aug 16 12:24:55 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,16 +4,23 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(final Integer number) {
         final String numStr = number.toString();
-        final boolean hasThree = numStr.contains("3");
-        final boolean hasFive = numStr.contains("5");
-        if((hasThree && hasFive) || (number % 3 == 0 && number % 5 == 0)) {
-            return "fizz buzz";
-        } else if(number % 3 == 0 || hasThree) {
+        boolean hasThree = numStr.contains("3");
+        boolean hasFive = numStr.contains("5");
+        boolean isDivisibleBy3 = number % 3 == 0;
+        boolean isDivisibleBy5 = number % 5 == 0;
+        boolean isFizz = isDivisibleBy3 || hasThree;
+        boolean isBuzz = isDivisibleBy5 || hasFive;
+        boolean isFizzBuzz = isDivisibleBy3 && isDivisibleBy5;
+        boolean isFizzAndBuzz = isFizz && isBuzz;
+
+        if(isFizzAndBuzz || isFizzBuzz){
+            return "fizzbuzz";
+        } else if(isFizz) {
             return "fizz";
-        } else if(number % 5 == 0 || hasFive) {
+        } else if (isBuzz) {
             return "buzz";
         }
-        return numStr;
+        return String.valueOf(number);
     }
 
 }
---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -0,0 +1,19 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzSolutionTest {
+
+    private FizzBuzzSolution sut;
+
+    @Before
+    public void setUp(){
+        this.sut = new FizzBuzzSolution();
+    }
+
+    @Test
+    public void shouldExpectFizzForMultiplesOf3 (){
+        Assertions.assertThat
+    }
+}
