59960938475f110f592ae310fb0fac485c9c962a
sbx_user1060 <sbx_user1060@ip-10-129-242-228.eu-west-2.compute.internal>
2018-08-16 14:20:07 +0000

    Thu Aug 16 14:20:07 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionHelperTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionHelperTest.java
@@ -1,4 +1,27 @@
 package befaster.solutions.FIZ;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
 public class FizzBuzzSolutionHelperTest {
+
+    private FizzBuzzSolutionHelper sut;
+
+    @Before
+    public void setUp() {
+        this.sut = new FizzBuzzSolutionHelper();
+    }
+
+    @Test
+    public void shouldExpectTrueOrFalseBasedOnInputProvided() {
+        Assertions.assertThat(this.sut.isDeluxe(11)).isTrue();
+        Assertions.assertThat(this.sut.isDeluxe(22)).isTrue();
+        Assertions.assertThat(this.numberService.isDeluxe(33)).isTrue();
+        Assertions.assertThat(this.numberService.isDeluxe(44)).isTrue();
+        Assertions.assertThat(this.numberService.isDeluxe(555)).isTrue();
+        Assertions.assertThat(this.numberService.isDeluxe(9999)).isTrue();
+        Assertions.assertThat(this.numberService.isDeluxe(223L)).isFalse();
+        Assertions.assertThat(this.numberService.isDeluxe(9989L)).isFalse();
+    }
 }
